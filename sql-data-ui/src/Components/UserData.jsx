a8d9f523cbd05531eaa06da00f7ad821ee4accdf
Panucio31 <m.januska1990@gmail.com>
2023-02-20 16:33:59 +0200

    function to use input value

--- sql-data-ui/src/Components/UserData.jsx
+++ sql-data-ui/src/Components/UserData.jsx
@@ -1,14 +1,40 @@
 function UserData() {
-  return ( 
-    <div className="data-table">
-      <table>
-        <th>
-          <th>hi</th>
-          <th>there</th>
-        </th>
-      </table>
+
+  const inputValue = (e) => {
+    const searchInput = e.target.value;
+    return searchInput;
+
+  }
+
+  const findUser = () => {
+    
+  }
+
+  return (
+    <div className="main-ui-container">
+      <div className="search-field">
+        <label htmlFor="search">Surasti Vartotoją</label>
+        <input type="text" name="search" onChange={(e) => inputValue(e)}/>
+        <button className="find-user" onClick={findUser}>Ieškoti</button>
+      </div>
+      
+        <tbody className="user-table">
+          <tr>
+            <th>vardas</th>
+            <th>pavarde</th>
+            <th>sutarties numeris</th>
+            <th>busena</th>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
     </div>
-   );
+      
+  );
 }
 
 export default UserData;
