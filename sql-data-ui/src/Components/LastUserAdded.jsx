112642bf6fd95e95bcc2b17a7a622b64fe578980
Panucio31 <m.januska1990@gmail.com>
2023-03-09 13:58:25 +0200

    filter function started

---
+++ sql-data-ui/src/Components/LastUserAdded.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LastUserAdded() {
+  return (
+    <div className='last-user'>
+      
+    </div>
+  )
+}
+
+export default LastUserAdded;
--- sql-data-ui/src/Components/UserData.jsx
+++ sql-data-ui/src/Components/UserData.jsx
@@ -1,31 +1,34 @@
 import { useState } from "react";
 import React from "react";
+import LastUserAdded from "./LastUserAdded";
 
 function UserData({ data }) {
   const [userData, setUserData] = useState([]);
-  const [contractNumber, setContractNumber] =useState()
-
-  
+  const [contractNumber, setContractNumber] = useState();
 
   const inputValue = (e) => {
     const searchValue = e.target.value;
     const initialState = [];
     setContractNumber(Number(searchValue));
     if (Number(searchValue) === 0) {
-      setContractNumber(initialState)
+      setContractNumber(initialState);
     }
-  }
+  };
 
-  console.log(contractNumber)
- 
+  console.log(contractNumber);
 
   const findUser = (e) => {
     e.preventDefault();
     const targetUser = data.find((user) => user.contract === contractNumber);
-    setUserData([...userData, targetUser])
-    console.log(userData.length)
+    setUserData([...userData, targetUser]);
+    console.log(userData.length);
   };
- 
+
+  const checkboxValues = () => {
+    let state = document.querySelector('filter-box-state');
+    console.log(state)
+  }
+
 
   // const findUser = () => {
   //   const searchData = inputValue();
@@ -42,9 +45,30 @@ function UserData({ data }) {
           onChange={(e) => inputValue(e)}
           placeholder="Vartotojo sutarties numeris"
         />
-        <button type="submit" className="find-user" onClick={(e) => findUser(e)}>
+        <button
+          type="submit"
+          className="find-user"
+          onClick={(e) => findUser(e)}
+        >
           Ieškoti
         </button>
+      </div>
+      <div className="info-container">
+        <LastUserAdded />
+      </div>
+      <div className="filter">
+        <div className="filter-box">
+          <label htmlFor="checkbox">Linijos Nr.</label>
+          <input type="checkbox" onClick={checkboxValues()}/>
+        </div>
+        <div className="filter-box-state">
+          <label htmlFor="checkbox">Būsena</label>
+          <input type="checkbox" />
+        </div>
+        <div className="filter-box-date">
+          <label htmlFor="checkbox">Aktyvavimo Data</label>
+          <input type="date" />
+        </div>
       </div>
       <table className="user-table">
         <tbody>
@@ -54,19 +78,21 @@ function UserData({ data }) {
             <th>Sutarties numeris</th>
             <th>Būsena</th>
           </tr>
-          {userData.length !== 0 ? userData.map((user) => (
-            <tr className="user-row" key={user.id}>
-              <td>{user.userName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.contract}</td>
-            </tr>
-          )) : data.map((user) => (
-            <tr className="user-row" key={user.id}>
-              <td>{user.userName}</td>
-              <td>{user.lastName}</td>
-              <td>{user.contract}</td>
-            </tr>
-          ))}
+          {userData.length !== 0
+            ? userData.map((user) => (
+                <tr className="user-row" key={user.id}>
+                  <td>{user.userName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.contract}</td>
+                </tr>
+              ))
+            : data.map((user) => (
+                <tr className="user-row" key={user.id}>
+                  <td>{user.userName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.contract}</td>
+                </tr>
+              ))}
         </tbody>
       </table>
     </div>
